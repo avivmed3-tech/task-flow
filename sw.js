0d8b368f4e86eb9c7aedac37540009ca8480f0eb
avivmed3-tech <avivmed3@gmail.com>
2026-03-17 14:39:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'taskflow-v14';
+const CACHE = 'taskflow-v15';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
